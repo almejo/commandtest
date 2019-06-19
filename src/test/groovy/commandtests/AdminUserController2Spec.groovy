399d19d3b5cfdb16777e92f05d3228b33de12933
Alejandro Vera <alejo@Alejandros-iMac.local>
2019-06-19 11:01:56 -0400

    Other example with ControllerUnitTest

---
+++ src/test/groovy/commandtests/AdminUserController2Spec.groovy
@@ -0,0 +1,34 @@
+package commandtests
+
+import grails.testing.web.controllers.ControllerUnitTest
+import org.apache.commons.lang.RandomStringUtils
+import spock.lang.Specification
+
+class AdminUserController2Spec extends Specification implements ControllerUnitTest<AdminUserController> {
+
+	void testUserCommand() {
+		when:
+		UserCommand command = new UserCommand()
+
+		then:
+		!command.validate()
+		command.errors['firstName'].code == 'nullable'
+
+		when:
+		command = new UserCommand(
+				firstName: RandomStringUtils.randomAlphabetic(256)
+		)
+
+		then:
+		!command.validate()
+		command.errors['firstName'].code == 'maxSize.exceeded'
+
+		when:
+		command = new UserCommand(
+				firstName: RandomStringUtils.randomAlphabetic(10)
+		)
+
+		then:
+		command.validate()
+	}
+}
